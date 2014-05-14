c172c96c3c16181c98a0f6263c592e72baced91c
Nathan Posey <nathan.posey@freescale.com>
2014-05-14 13:42:19 -0500

    Update options script to support k24s and easier merge format

--- tools/options.py
+++ tools/options.py
@@ -16,8 +16,18 @@ limitations under the License.
 """
 from optparse import OptionParser
 
-INTERFACES = ['LPC11U35', 'K20DX128', 'LPC4322']
-TARGETS = ['LPC812', 'LPC11U68', 'LPC1549', 'LPC1768', 'KL25Z', 'K64F', 'K22F']
+INTERFACES = ['LPC11U35',
+              'K20DX128',
+              'LPC4322']
+
+TARGETS = ['LPC812',
+           'LPC11U68',
+           'LPC1549',
+           'LPC1768',
+           'KL25Z',
+           'K64F',
+           'K22F',
+           'K24F256']
 
 
 def options_parser():
